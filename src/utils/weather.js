4fb73091a1031ca8e81e67519f3eceb93e0bcad5
Karen O'Krafka <kokrafka@opentext.com>
2024-10-08 16:50:24 -0400

    added additional forecast data

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,5 +1,11 @@
 const request = require("request");
 
+// add additional data
+// update forecase string to include new data
+// commit your changes
+// push the changes to github and deploy
+// test your work
+
 const getWeather = (lat, lng, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=4aaca9aa417790bd8522fe002b66bd5d&query=" +
@@ -19,7 +25,16 @@ const getWeather = (lat, lng, callback) => {
       const currentTemperature = body.current.temperature;
       const feelsLikeTemperature = body.current.feelslike;
       const weatherDescription = body.current.weather_descriptions[0];
-      const forecast = `${weatherDescription}. It is currently ${currentTemperature} degrees outside. It feels like ${feelsLikeTemperature} degrees outside.\n`;
+      const humidity = body.current.humidity;
+      const uvindex = body.current.uv_index;
+      const windspeed = body.current.wind_speed;
+
+      const additionalForecastData = `The humdity is ${humidity}%. The wind speed is ${windspeed}km per hour.\n`;
+
+      const forecast =
+        `${weatherDescription}. It is currently ${currentTemperature} degrees outside. It feels like ${feelsLikeTemperature} degrees outside with a UV index of ${uvindex}.\n\n` +
+        additionalForecastData;
+
       callback(undefined, forecast);
     }
   });
